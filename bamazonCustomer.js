e64a42bd8fb85ce961b427b6a326eec8c99f543a
Dean Mamnev <deanmamnev@gmail.com>
2018-04-09 21:10:01 -0500

    Wayne Static says DON'T PUSH IT

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,3 +12,50 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+
+    // displayBootUp();
+    console.log("Displaying all products");
+
+    console.log("#" + " |" + "Item ID#" + " |" + "Product Name" + " |" + "Department" + " |" + "Price" + " |" + "Stock Quantity");
+    queryAllProducts();
+    promptBuy();
+    connection.end();
+});
+
+
+
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " |" + res[i].item_id + " |" + res[i].product_name + " |" + res[i].department_name + " |" + res[i].price + " |" + res[i].stock_quantity);
+        }
+        console.log("--------------------------------");
+    });
+}
+
+function promptBuy() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Whaddya wanna buy?  Enter ID#.",
+            name: "input",
+        }
+    ])
+    .then(
+        function (response){
+            console.log(response.input);
+            
+                connection.query("select * from products where item_id='"+ response.input + "'", function (err, res) {
+                    console.log("Searching for:" + response.input);
+                    for (var i = 0; i < res.length; i++) {
+                        console.log(res[i].id + " |" + res[i].item_id + " |" + res[i].product_name + " |" + res[i].department_name + " |" + res[i].price + " |" + res[i].stock_quantity);
+                    }
+                    console.log("--------------------------------");
+                });
+            
+        }
+    )
+}
